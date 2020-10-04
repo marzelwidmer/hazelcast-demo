842f83f7a2595e2837916ac2860e8e2c30824636
Marcel Widmer <marzelwidmer@gmail.com>
2020-10-04 18:27:44 +0200

    K8s Hazelcast

---
+++ src/test/kotlin/ch/keepcalm/demo/hazelcast/DemoHazelcastApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.keepcalm.demo.hazelcast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoHazelcastApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
